be819ce1100be66885da0324dc66013f6a8d495c
rosegithub-dotcom <143061942+rosegithub-dotcom@users.noreply.github.com>
2023-11-09 13:36:47 +0300

    OOP Assignment

---
+++ Question1.py
@@ -0,0 +1,33 @@
+#CreatE a user class with properties
+# name, age, location
+
+class User:
+    def _init_(User,name,age,location):
+        User.name=name
+        User.age=age
+        User.location=location
+name=input("Enter your name:")
+age=input("Enter your age:")
+location=input("Enter your location:")
+print(User)
+
+#Creating new instance of the User class
+#Access the user's name and age
+
+class User:
+    def _init_(User,name,age):
+        User.name=name
+        User.age=age
+name=input("Enter your name:")
+age=input("Enter your age:")
+print(name)
+print(age)
+
+#Function that prints a User's location
+#Print the user's location using the function
+
+class User:
+    def __init__(User,location):
+         User.location=location
+location=input("Enter your location:")
+print(location)
